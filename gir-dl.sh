a4edd86bcb87e7d82e5ef0c4da3c8c65c388a0ca
Evgenii Pashkin <eapashkin@gmail.com>
2017-08-25 22:51:57 +0300

    Added script for group upload, downloaded latest zesty versions

--- gir-dl.sh
+++ gir-dl.sh
@@ -3,7 +3,16 @@
 set -e
 set -u
 
-URL="$1"
+MIRRORS="$1"
+if [ $# -lt 2 ]; then
+  MIRROR="mirrors.kernel.org"
+else
+  MIRROR="$2"
+fi
+wget -q --show-progress -O tmp.html "$MIRRORS"
+URL=`cat tmp.html | grep -oP "http://$MIRROR/[^\"]+"`
+rm tmp.html
+echo $URL
 wget -q --show-progress -O tmp.deb "$URL"
 ar x tmp.deb data.tar.xz
 rm tmp.deb
