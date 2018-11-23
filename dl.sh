34b13640adc18c9d3eefb8e03c9c5522f09001fc
Evgenii Pashkin <eapashkin@gmail.com>
2018-11-23 19:42:47 +0300

    Undo name fix for Cairo.FontType

--- dl.sh
+++ dl.sh
@@ -5,7 +5,7 @@ VER="bionic"
 
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libatk1.0-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libgirepository1.0-dev/download
-./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libpango1.0-dev/download
+./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libpango1.0-dev/download security.ubuntu.com/ubuntu
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libgdk-pixbuf2.0-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libgtk-3-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libgtksourceview-3.0-dev/download
