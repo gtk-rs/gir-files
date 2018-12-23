098a25403099395a9498109d35e33c4bbaa2fefd
Mathieu Bridon <bochecha@daitauha.fr>
2018-12-23 21:11:45 +0100

    Add the IBus gir file

--- dl.sh
+++ dl.sh
@@ -9,6 +9,7 @@ VER="bionic"
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libgdk-pixbuf2.0-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libgtk-3-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libgtksourceview-3.0-dev/download
+./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libibus-1.0-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libsecret-1-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libvte-2.91-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libjavascriptcoregtk-4.0-dev/download security.ubuntu.com/ubuntu
