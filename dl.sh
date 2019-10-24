44fb091a3edad03f9b9805e3c14f334ce8b9fb84
Velislava Yanchina <vel.yanchina@yahoo.com>
2019-10-24 16:59:37 +1100

    Revert "update dl.sh"
    This reverts commit 10cbf66a2beb96a3a37742c86fc94087bc0421d8.

--- dl.sh
+++ dl.sh
@@ -5,6 +5,7 @@ VER="eoan"
 
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libatk1.0-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libgirepository1.0-dev/download
+./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libpango1.0-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libgdk-pixbuf2.0-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libgtk-3-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libgtksourceview-3.0-dev/download
@@ -13,6 +14,6 @@ VER="eoan"
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libjavascriptcoregtk-4.0-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libsoup2.4-dev/download
 ./gir-dl.sh https://packages.ubuntu.com/$VER/amd64/libwebkit2gtk-4.0-dev/download
-./gir-dl.sh https://packages.debian.org/experimental/amd64/libpango1.0-dev/download
+
 
 ./reformat.sh
