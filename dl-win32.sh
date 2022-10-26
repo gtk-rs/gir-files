00f92a2be4a3e477fb66377c3ebbce83d82064e3
Marc-André Lureau <marcandre.lureau@redhat.com>
2022-10-26 11:25:01 +0400

    dl-win32.sh: take the last version from msys2
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- dl-win32.sh
+++ dl-win32.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-GTK_VERSION=4.8.1-1
+GTK_VERSION=$(wget -qO- "https://packages.msys2.org/api/search?query=gtk4" | jq -r ".results.exact.version")
 
-wget -qO- https://mirror.msys2.org/mingw/ucrt64/mingw-w64-ucrt-x86_64-gtk4-$GTK_VERSION-any.pkg.tar.zst | \
-    zstdcat - | tar xO ucrt64/share/gir-1.0/GdkWin32-4.0.gir > GdkWin32-4.0.gir
+wget -qO- "https://mirror.msys2.org/mingw/ucrt64/mingw-w64-ucrt-x86_64-gtk4-$GTK_VERSION-any.pkg.tar.zst" | \
+    zstdcat - | tar xO "ucrt64/share/gir-1.0/GdkWin32-4.0.gir" > GdkWin32-4.0.gir
