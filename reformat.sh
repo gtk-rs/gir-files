d2d852e0a57d066f451145cd1041dee9061ee21c
Maximiliano Sandoval <msandova@protonmail.com>
2026-07-26 15:19:29 +0200

    reformat: Encode gir files as utf-8
    Introspection data files are utf-8, but xmlstarlet edit would assume
    they are not since they do not have encoding=utf-8 in their xml
    declaration. We run this extra 'format' command to insert the encoding
    so that the files are not mangled by 'edit'.

--- reformat.sh
+++ reformat.sh
@@ -3,9 +3,13 @@ set -x -e
 
 # `///` used as `//` not works in Windows in this case
 for file in *.gir; do
-	xmlstarlet edit --inplace \
+    # TODO Revert once all files have been regenerated with
+    # https://gitlab.gnome.org/GNOME/gobject-introspection/-/merge_requests/558.
+	xmlstarlet format --encode utf-8 "$file" \
+	| xmlstarlet edit \
 		--delete '//_:doc/@line' \
 		--delete '//_:doc/@filename' \
 		--delete '///_:source-position' \
-		"$file"
+		> "$file".tmp
+	/usr/bin/mv "$file".tmp "$file"
 done
